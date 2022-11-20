3f7105a3653ce28dbe5c67998c08e1b385f21b8f
KOgundeji <97274909+KOgundeji@users.noreply.github.com>
2022-11-20 15:43:19 -0800

    Added Menu

--- app/src/main/java/com/kunle/shoppinglistapp/MainActivity.java
+++ app/src/main/java/com/kunle/shoppinglistapp/MainActivity.java
@@ -1,21 +1,49 @@
 package com.kunle.shoppinglistapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.splashscreen.SplashScreen;
+import androidx.databinding.DataBindingUtil;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout left_nav;
+    private ActionBarDrawerToggle actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
-//        try {
-//            Thread.sleep(3000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
+//        wait(3000);
         setContentView(R.layout.activity_main);
+        left_nav = findViewById(R.id.my_drawer_layout);
+        actionBar = new ActionBarDrawerToggle(this,left_nav,R.string.nav_open,R.string.nav_close);
+
+        left_nav.addDrawerListener(actionBar);
+        actionBar.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (actionBar.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void wait(int milliseconds) {
+        try {
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
